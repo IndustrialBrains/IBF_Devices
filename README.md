2e8d83cfaf26a146f86c418c68fa1153eabbd1b2
Justus Rijke <justus@industrialbrains.nl>
2022-11-15 19:09:23 +0100

    added more tests

--- README.md
+++ README.md
@@ -8,11 +8,12 @@
 
 1. Add build action
 1. Add automatic tests
-1. Rename project
-1. (check) force use of access specifiers
 
 ### Improvements
 
-1. `Fb_DevAxis`: Fb_DevAxis.Init VAR_INPUT default values have no effect (move defaults to Fb_DevAxis VARs or use PROPERTY)
+1. `Fb_DevAxis`:
+	- Fb_DevAxis.Init VAR_INPUT default values have no effect (move defaults to Fb_DevAxis VARs or use PROPERTY)
+	- Add feature: check if online motor parameters match project parameters
+	- When STO is not OK, no fault/error is generated; this is as designed (handled by higher level safety system), but is not very intuitive
 
 
--- Tests/test_fb_devaxis.py
+++ Tests/test_fb_devaxis.py
@@ -1,14 +1,16 @@
-"""Tests for Fb_DevAxis"""
+"""
+Tests for Fb_DevAxis
+"""
 # pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name
 import sys
 import unittest
 
-from connection import cold_reset, conn, wait_cycles
+from connection import cold_reset, conn, wait_cycles, wait_value
 
 COLD_RESET = True
 
 
-class TestFb_DevAxis(unittest.TestCase):
+class Tests(unittest.TestCase):
 
     PREFIX = "PRG_TEST_FB_DEVAXIS"
 
@@ -24,24 +26,50 @@ class TestFb_DevAxis(unittest.TestCase):
     def setUp(self) -> None:
         if COLD_RESET:
             cold_reset()
+
         return super().setUp()
 
-    @staticmethod
-    def _trigger_falling_edge(var: str) -> None:
-        conn.write_by_name(var, True)
-        wait_cycles(1)
-        conn.write_by_name(var, False)
-        wait_cycles(1)
+    def _init(self):
+        conn.write_by_name(f"{self.PREFIX}.bInit", True)
+        self.assertTrue(wait_value(f"{self.PREFIX}.bInit", False, 2))
+        conn.write_by_name(f"{self.PREFIX}.fbDevAxis.IAxisSTOOK", True)
 
-    @staticmethod
-    def _trigger_rising_edge(var: str) -> None:
-        conn.write_by_name(var, False)
-        wait_cycles(1)
-        conn.write_by_name(var, True)
-        wait_cycles(1)
+        # The simulated axis retains its state after a cold reset.
+        # Therefore, a Reset is triggered after init.
+        # NOTE: drive parameter changes are retained after the reset!
+        conn.write_by_name(f"{self.PREFIX}.bCmdReset", True)
+        self.assertTrue(wait_value(f"{self.PREFIX}.bCmdReset", False, 2))
+
+    def _home(self):
+        """Homing sequence based on MC_HomingMode.MC_DefaultHoming with a N/O reference sensor"""
+        self._init()
+        conn.write_by_name(f"{self.PREFIX}.fbDevAxis.IAxisHome", True)
+        conn.write_by_name(f"{self.PREFIX}.bCmdHome", True)
+        wait_cycles(100)
+        conn.write_by_name(f"{self.PREFIX}.fbDevAxis.IAxisHome", False)
+        self.assertTrue(wait_value(f"{self.PREFIX}.bCmdHome", False, 2))
 
     def test_00_init(self):
-        self.assertFalse(conn.read_by_name(f"{self.PREFIX}.bInitBusy"))
+        self._init()
+
+    def test_01_CmdHome(self):
+        self._home()
+
+    def test_CmdHold_during_homing(self):
+        self._init()
+        conn.write_by_name(f"{self.PREFIX}.bCmdHome", True)
+        wait_cycles(100)
+        conn.write_by_name(f"{self.PREFIX}.bCmdHold", True)
+        wait_cycles(100)
+        self.assertEqual(conn.read_by_name(f"{self.PREFIX}.fbDevAxis.fVelocity"), 0)
+
+    def test_CmdHold_during_movement(self):
+        self._home()
+        conn.write_by_name(f"{self.PREFIX}.bCmdMoveAbs", True)
+        wait_cycles(50)
+        conn.write_by_name(f"{self.PREFIX}.bCmdHold", True)
+        wait_cycles(100)
+        self.assertEqual(conn.read_by_name(f"{self.PREFIX}.fbDevAxis.fVelocity"), 0)
 
 
 if __name__ == "__main__":
