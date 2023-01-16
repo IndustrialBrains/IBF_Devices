8fefb646365cea5b9058bee86ee6bf58d983c404
Justus Rijke <justus@industrialbrains.nl>
2023-01-16 13:56:08 +0100

    cleaned up FB_ManualController interface

--- Tests/test_FB_ManualController.py
+++ Tests/test_FB_ManualController.py
@@ -5,8 +5,6 @@ import sys
 import unittest
 
 # pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name
-from os import device_encoding
-
 from connection import cold_reset, conn, wait_cycles, wait_value
 
 COLD_RESET = True
@@ -15,7 +13,7 @@ COLD_RESET = True
 class Tests(unittest.TestCase):
 
     PREFIX = "PRG_TEST_FB_MANUALCONTROLLER"
-    PREFIX_DEVICE = f"{PREFIX}.FbDevValve"
+    PREFIX_DEVICE = f"{PREFIX}.fbDevValve"
     PREFIX_CONTROLLER = f"{PREFIX}.fbManualController"
 
     @classmethod
@@ -33,8 +31,13 @@ class Tests(unittest.TestCase):
         conn.write_by_name(f"{self.PREFIX}.bEnableTests", True)
         return super().setUp()
 
+    def _enable(self) -> None:
+        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.nSelectedDevice", 1)
+        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.bEnabled", True)
+        self.assertTrue(wait_value(f"{self.PREFIX_DEVICE}.bManual", True, 1))
+
     def test_01_device_added(self) -> None:
-        device_name = conn.read_by_name(f"{self.PREFIX_DEVICE}.sName")
+        device_name = conn.read_by_name(f"{self.PREFIX_DEVICE}.stIdentity.sName")
         # NOTE: device will be added at index 1, as index 0 is reserved for a dummy device ("No device")
         device_in_array = conn.read_by_name(
             f"{self.PREFIX_CONTROLLER}.arDeviceArray[1].sName"
@@ -42,15 +45,17 @@ class Tests(unittest.TestCase):
         self.assertEqual(device_name, device_in_array)
 
     def test_02_enable_manual(self) -> None:
-        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.nSelectedDevice", 1)
-        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.bEnabled", True)
+        self._enable()
         self.assertTrue(wait_value(f"{self.PREFIX_DEVICE}.bManual", True, 1))
 
     def test_03_disable_manual(self) -> None:
-        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.nSelectedDevice", 1)
-        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.bEnabled", True)
-        self.assertTrue(wait_value(f"{self.PREFIX_DEVICE}.bManual", True, 1))
+        self._enable()
         conn.write_by_name(f"{self.PREFIX_CONTROLLER}.stHMI.bEnabled", False)
+        self.assertTrue(wait_value(f"{self.PREFIX_DEVICE}.bManual", False, 1))
+
+    def test_04_block_manual(self) -> None:
+        self._enable()
+        conn.write_by_name(f"{self.PREFIX_CONTROLLER}.bBlockManualMode", True)
         self.assertTrue(wait_value(f"{self.PREFIX_DEVICE}.bManual", False, 1))
 
 
